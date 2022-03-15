85b916632cc218e138245412bb8adc6ecd9ddfe4
Robert Fischer <robert.fischer@empa.ch>
2022-03-15 16:19:24 +0100

    cleaned up a little

--- testanimscript.py
+++ testanimscript.py
@@ -3,11 +3,6 @@ import os
 
 baseFolder=r"A:\anim_test"
 
-# bpy.context.scene.render.filepath = r"Z:\users\firo\blender\test\test_water3.png"
-# bpy.ops.render.render(write_still=True)
-
-# print('Warning: this is a crude hack. Make sure the names are capturing the correct objects')
-
 #delete old watermesh
 name = 'watersmooth'
 bpy.ops.object.select_all(action='DESELECT')
@@ -15,15 +10,8 @@ bpy.data.objects[name].select_set(True)
 bpy.ops.object.delete(confirm=False)
 
 
-# #import new mesh
-# name = 'watertest'
-# bpy.ops.import_mesh.stl(filepath=r"A:\watertest.stl")
-
 mat = bpy.data.materials['water']
-# bpy.data.objects[name].data.materials.append(mat)
 
-# bpy.context.scene.render.filepath = r"Z:\users\firo\blender\test\test_water_not_smooth.png"
-# bpy.ops.render.render(write_still=True)
 
 def load_and_render_stl(filename, baseFolder=baseFolder, mat=mat, outfolder=r"A:\anim_test_png"):
 	name = filename[:-4]
