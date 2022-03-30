a72762cd9f392c1ff2c1d7399260d8939f0848ed
Robert Fischer <robert.fischer@empa.ch>
2022-03-30 14:06:47 +0200

    test interlace video and drafted logo

--- 02_adjust_variable_frame_rate.py
+++ 02_adjust_variable_frame_rate.py
@@ -14,13 +14,13 @@ import shutil
 
 nc_path = r"Z:\Robert_TOMCAT_3_combined_archives\unmasked\dyn_data_T3_025_3_III.nc"
 
-timestep_folder = r"Z:\anim_test_full_png2"
+timestep_folder = r"Z:\anim_test_full_png_int"
 time_folder = r"R:\Scratch\305\_Robert\time_test"
 
 if not os.path.exists(time_folder):
     os.makedir(time_folder)
 
-fileprefix = 'water_'
+fileprefix = 'water_ts_'
 pfx = len(fileprefix)
 
 filesuffix = '.png'
@@ -34,14 +34,39 @@ dyn_data.close()
 # temporary solution: manually define time steps and framerate
 
 def move_file(ts, folder):
-    filename = ''.join([fileprefix,str(ts), filesuffix])
+    filename = ''.join([fileprefix,f'{ts:05}', filesuffix])
     filepath = os.path.join(timestep_folder, filename)
     if os.path.exists(filepath):
         shutil.move(filepath, os.path.join(folder,filename))
 
 # part1 6s per scan
-part1_steps = np.arange(101)
+# part1_steps = np.arange(101)
 
+# part1_folder = os.path.join(timestep_folder, 'part1')
+# if not os.path.exists(part1_folder):
+#     os.mkdir(part1_folder)
+
+# for ts in part1_steps:
+#     move_file(ts, part1_folder)
+    
+# # part2 1s per scan
+# part2_steps = np.arange(101,220)
+# part2_folder = os.path.join(timestep_folder, 'part2')
+# if not os.path.exists(part2_folder):
+#     os.mkdir(part2_folder)
+# for ts in part2_steps:
+#     move_file(ts, part2_folder)
+
+# # part3 7s per scan
+# part3_steps = np.arange(220, len(exp_time))
+# part3_folder = os.path.join(timestep_folder, 'part3')
+# if not os.path.exists(part3_folder):
+#     os.mkdir(part3_folder)
+# for ts in part3_steps:
+#     move_file(ts, part3_folder)
+
+# part1 0.4s per scan
+part1_steps = np.arange(180)
 part1_folder = os.path.join(timestep_folder, 'part1')
 if not os.path.exists(part1_folder):
     os.mkdir(part1_folder)
@@ -49,19 +74,11 @@ if not os.path.exists(part1_folder):
 for ts in part1_steps:
     move_file(ts, part1_folder)
     
-# part2 1s per scan
-part2_steps = np.arange(101,220)
+# part2 0.8s per scan
+part2_steps = np.arange(180,271)
 part2_folder = os.path.join(timestep_folder, 'part2')
 if not os.path.exists(part2_folder):
     os.mkdir(part2_folder)
 for ts in part2_steps:
     move_file(ts, part2_folder)
-
-# part3 7s per scan
-part3_steps = np.arange(220, len(exp_time))
-part3_folder = os.path.join(timestep_folder, 'part3')
-if not os.path.exists(part3_folder):
-    os.mkdir(part3_folder)
-for ts in part3_steps:
-    move_file(ts, part3_folder)
     
