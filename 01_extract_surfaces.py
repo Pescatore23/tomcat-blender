053a12f8b896685b5654e9ae724698e8bd7506bf
Fischer Robert <robert.fischer@psi.ch>
2023-02-03 10:50:15 +0100

    added GPU image cleaning and image pre-cropping

--- 01_extract_surfaces.py
+++ 01_extract_surfaces.py
@@ -18,11 +18,48 @@ from scipy import ndimage
 host = socket.gethostname()
 
 
+
+# TODO: make this more elegant
+try:
+    import cupy as cp
+    import cucim
+    GPU_avail = True
+except:
+    print('cupy and cucim not found')
+    GPU_avail = False
+
+
 # TODO: add GPU support once cucim/cupy allow GPU marching cubes
-# TODO: already allow cupy for matrix operations and loading
+# TODO: already allow cupy for matrix operations and loading, image cleaning already possible with GPU
 temp_folder = None
 if host == 'DDM06609':
     temp_folder = r"Z:\users\firo\joblib_tmp"
+    
+num_GPU = 1
+if host == 'mpc2959.psi.ch':
+    num_GPU = 5
+    
+    
+
+def clean_binary_image(im, clean = True, remove_small=True,  minsize = 200, fp_radius = 1, i=0, GPU = True, GPU_avail=GPU_avail):
+    # GPU?
+    if clean or remove_small:
+        if GPU and GPU_avail:
+            gpu_id = i%num_GPU #use gpus 1 through 4, leaving the big A40 (0) alone or i%5 to use all 5
+    
+            with cp.cuda.Device(gpu_id):
+                im = cp.array(im)
+                if clean: im = cucim.skimage.morphology.binary_opening(im, footprint=cucim.skimage.morphology.ball(fp_radius))
+                if remove_small: im = cucim.skimage.morphology.remove_small_objects(im, min_size=minsize)
+                im = cp.asnumpy(im)
+                mempool = cp.get_default_memory_pool()
+                mempool.free_all_blocks()
+            
+        else: 
+            if clean: im = ndimage.binary_opening(im, structure=ball(fp_radius))
+            if remove_small: im = morphology.remove_small_objects(im, min_size=minsize)
+        
+    return im
 
 
 class mesh_maker:
@@ -31,6 +68,8 @@ class mesh_maker:
                  out_path = None,
                  ref_timestep = 50,
                  timesteps = None, #'all', or array
+                 x_boundaries = (0,-1),
+                 y_boundaries = (0,-1),
                  z_boundaries = (0,-1),
                  use_gpu = False, #stub to call either cupy/cucim or not,
                  cpu_parallel = True,
@@ -39,8 +78,13 @@ class mesh_maker:
         self.nc_path = nc_path
         self.out_path = out_path
             
-        self.zbot =  z_boundaries[0]
+        self.xbot = x_boundaries[0]
+        self.xtop = x_boundaries[1]
+        self.ybot = z_boundaries[0]
+        self.ytop = z_boundaries[1]
+        self.zbot = z_boundaries[0]
         self.ztop = z_boundaries[1]
+        
         self.ref_ts = ref_timestep
         self.timesteps = timesteps
         self.use_gpu = use_gpu
@@ -50,7 +94,7 @@ class mesh_maker:
     def load_data(self):
         self.dyn_data = xr.open_dataset(self.nc_path)
         if 'transition_matrix' in list(self.dyn_data.keys()):
-            self.watermask = (self.dyn_data['transition_matrix']>0).data[:,:,self.zbot:self.ztop]
+            self.watermask = (self.dyn_data['transition_matrix']>0).data[self.xbot:self.xtop,self.ybot:self.ytop,self.zbot:self.ztop]
         if not os.path.exists(self.out_path):
             os.mkdir(self.out_path)
 
@@ -58,7 +102,7 @@ class mesh_maker:
     def time_step_stl(self, ts):
         
         water = self.dyn_data['transition_matrix']<ts
-        water = np.bitwise_and(water[:,:,self.zbot:self.ztop], self.watermask)
+        water = np.bitwise_and(water[self.xbot:self.xtop,self.ybot:self.ytop,self.zbot:self.ztop], self.watermask)
         
         if np.any(water):
             water[:,:,:2] = 0
@@ -71,16 +115,26 @@ class mesh_maker:
             with open(stlpath, 'w+') as file: 
                  file.write(stl) 
                  
-    def time_4D_stl(self, ts, phase=1, phasename = 'phase_1', clean=False, remove_small= False, fp = None, minsize=20):
+    def time_4D_stl(self, ts, phase=1, phasename = 'phase_1', clean=False, remove_small= False, fp_radius = 1, minsize=20):
         
         im = self.dyn_data['segmented'].sel(time=ts)==phase
         
         if np.any(im):
-            im = im.data
-            if clean: im = ndimage.binary_opening(im, structure=fp)
-            if remove_small: im = morphology.remove_small_objects(im, min_size=minsize)
+            im = im[self.xbot:self.xtop,self.ybot:self.ytop,self.zbot:self.ztop].data
+            # TODO: use GPU
+            # if clean: im = ndimage.binary_opening(im, structure=fp)
+            # if remove_small: im = morphology.remove_small_objects(im, min_size=minsize)
+            
+            #crop boundaries
+            im[:,:2,:] = 0
+            im[:,-3:,:] = 0
+            im[:2,:,:] = 0
+            im[-3:,:,:] = 0
             im[:,:,:2] = 0
             im[:,:,-3:] = 0
+            
+            im = clean_binary_image(im, clean, remove_small,  minsize, fp_radius = 1, i=ts)
+                        
             verts, faces, _, _ = measure.marching_cubes(im) #_lewiner
             mesh = trimesh.Trimesh(vertices = verts, faces = faces)
             stl = trimesh.exchange.stl.export_stl_ascii(mesh)
@@ -115,7 +169,7 @@ class mesh_maker:
             self.dyn_data.close()
         self.dyn_data.close()
         
-    def run2(self, phase, name, clean=False, remove_small=False, fp=ball(1), minsize=20):
+    def run2(self, phase, name, clean=False, remove_small=False, fp_radius=1, minsize=20):
         # laod the data
         self.load_data()
        # TODO:close ncfile if error
@@ -131,7 +185,7 @@ class mesh_maker:
                     steps = self.timesteps
                 
                 if self.parallel:
-                    Parallel(n_jobs=self.njobs, temp_folder=temp_folder)(delayed(self.time_4D_stl)(ts, phase, name, clean,remove_small, fp, minsize) for ts in steps)
+                    Parallel(n_jobs=self.njobs, temp_folder=temp_folder)(delayed(self.time_4D_stl)(ts, phase, name, clean,remove_small, fp_radius, minsize) for ts in steps)
                 else:
                     for ts in steps:
                         if not ts == self.ref_ts:
