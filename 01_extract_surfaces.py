1d23468014d4c95f02f4c099b8f6724baeca26e9
Fischer Robert <robert.fischer@psi.ch>
2023-05-16 16:42:29 +0200

    stupid

--- 01_extract_surfaces.py
+++ 01_extract_surfaces.py
@@ -119,7 +119,7 @@ class mesh_maker:
                  
     def time_4D_stl(self, ts, phase=1, phasename = 'phase_1', clean=False, remove_small= False, fp_radius = 1, minsize=20):
         
-        im = self.dyn_data['segmented'].sel(time=ts)[self.xbot:self.xtop,self.ybot:self.ytop,self.zbot:self.ztop]==phase
+        # im = self.dyn_data['segmented'].sel(time=ts)[self.xbot:self.xtop,self.ybot:self.ytop,self.zbot:self.ztop]==phase
         im = self.dyn_data['segmented'].sel(timestep=ts)[self.xbot:self.xtop,self.ybot:self.ytop,self.zbot:self.ztop]==phase
         
         if np.any(im):
