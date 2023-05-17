9cf2724418f80828f2192526fe452cbd458ff3dd
Fischer Robert <fische_r@mpc2959.psi.ch>
2023-05-17 13:16:29 +0200

    do not overwerite existing files

--- animscript_full_crack.py
+++ animscript_full_crack.py
@@ -2,7 +2,7 @@ import bpy
 import os
 
 baseFolder="/mpc/homes/fische_r/NAS/DASCOELY/processing/04_membrane_ML/3II/crack/" #for stl
-outfolder="/mpc/homes/fische_r/NAS/DASCOELY/processing/04_membrane_ML/3II/crack_cathode_camera_png/" #for png
+outfolder="/mpc/homes/fische_r/NAS/DASCOELY/processing/04_membrane_ML/3II/crack_anode_camera_png/" #for png
 
 material = 'crack' #'Crack'
 
@@ -24,17 +24,18 @@ mat = bpy.data.materials[material]
 def load_and_render_stl(filename, baseFolder=baseFolder, mat=mat, outfolder=outfolder):
 	name = filename[:-4]
 	
-	bpy.ops.import_mesh.stl(filepath=os.path.join(baseFolder, filename), axis_up='-Z')
-	bpy.data.objects[name].data.materials.append(mat)
+        outfile = os.path.join(outfolder, ''.join([name,'.png']))
+        if not os.path.exists(outfile):
+
+		bpy.ops.import_mesh.stl(filepath=os.path.join(baseFolder, filename), axis_up='-Z')
+		bpy.data.objects[name].data.materials.append(mat)
+		bpy.context.scene.render.filepath = outfile
+		bpy.ops.render.render(write_still=True)
 	
-	outfile = os.path.join(outfolder, ''.join([name,'.png']))
-	bpy.context.scene.render.filepath = outfile
-	bpy.ops.render.render(write_still=True)
-	
-	#clean up
-	bpy.ops.object.select_all(action='DESELECT')
-	bpy.data.objects[name].select_set(True)
-	bpy.ops.object.delete(confirm=False)
+		#clean up
+		bpy.ops.object.select_all(action='DESELECT')
+		bpy.data.objects[name].select_set(True)
+		bpy.ops.object.delete(confirm=False)
 	
 filenames = os.listdir(baseFolder)
 
