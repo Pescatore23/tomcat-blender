a473355966b06179722f61396bf303076067ae83
Fischer Robert <robert.fischer@psi.ch>
2024-01-24 10:16:07 +0100

    need to convert str to bool

--- 03_nc_to_npy_for_openvdb.py
+++ 03_nc_to_npy_for_openvdb.py
@@ -40,6 +40,14 @@ if host == 'mpc2959.psi.ch':
     num_GPU = 5
     n_jobs = 16
     
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
     
     
 class volume_maker:
